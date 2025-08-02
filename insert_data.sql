10dfa597e5767e439c2cc321c9104061c8f264b2
ashwinitadkale <ashwinitadkale@gmail.com>
2025-08-02 23:29:43 +0530

    Create insert_data.sql

---
+++ insert_data.sql
@@ -0,0 +1,11 @@
+-- Books
+INSERT INTO books (title, author, genre, total_copies, available_copies)
+VALUES 
+('Atomic Habits', 'James Clear', 'Self-help', 5, 5),
+('The Alchemist', 'Paulo Coelho', 'Fiction', 3, 3);
+
+-- Members
+INSERT INTO members (name, email, phone)
+VALUES
+('Ashwini Tadkale', 'ashwini@example.com', '9876543210'),
+('Ravi Kumar', 'ravi@example.com', '9123456789');
